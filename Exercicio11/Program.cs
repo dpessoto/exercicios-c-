fe2b5e3c262e0ae6f23d0f833e8cd12e04aa8d06
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-11 13:26:58 -0300

    Exercicio 11

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio11 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+
+    class Alvo {
+        public virtual void Requisitar() {
+            Console.WriteLine("Alvo");
+        }
+    }
+    class Ajustar : Alvo {
+        Ajustado _adaptee = new Ajustado();
+        public override void Requisitar() {
+            _adaptee.SpecificRequest();
+        }
+    }
+    class Ajustado {
+        public void SpecificRequest() {
+            Console.WriteLine("Requititado()");
+        }
+    }
+}
