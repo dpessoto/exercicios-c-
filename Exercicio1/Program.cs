94f12f06c85c468d27258df122b29ce4e7dda04d
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-11 08:53:02 -0300

    initial commit / Exercicio 1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio1 {
+
+    public class Calc {
+        public int CalcY(int p1, int p2 = 3, int p3 = 4) {
+            int ret;//= int.MinValue;
+            ret = p2 - p1 + p3;
+            return ret;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            Calc calc = new Calc();
+
+            calc.CalcY(); //precisa de argumento
+            calc.CalcY(1); //1 aurgumento é obrigatorio
+            calc.CalcY(1,2); //ter um segundo argumento é opcional
+            calc.CalcY(1, 2, 3); //ter um terceiro argumento é opcional
+
+            Console.WriteLine(calc.CalcY(1));
+        }
+
+    }
+}
