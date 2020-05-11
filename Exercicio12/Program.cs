41ccc04d446cc64d5b6bb22dbf321e4a4bebf466
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-11 13:32:42 -0300

    Exercicio 12

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio12 {
+    class Program {
+        static void Main(string[] args) {
+            int[] lista;
+            lista = new int[]
+            { 1, 2, 3, 4, 5, 6 };
+            int k14 = 10;
+            foreach (int num in lista) {
+                k14 -= num;
+                if (k14 % 2 == 0)
+                    break;
+            }
+            Console.WriteLine("K14={0}", ++k14);
+        }
+    }
+}
