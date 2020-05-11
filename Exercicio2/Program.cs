0e2cc953f8895a67472c81b0d77e8711cabc0272
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-11 09:05:28 -0300

    Exercio 2

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio1 {
+
+    public class Calc {
+        public int CalcY(int p1, int p2 = 3, int p3 = 4) {
+            int ret;//= int.MinValue;
+            ret = p2 - p1 + p3;
+            return ret;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            Calc obj1 = new Calc();
+
+            obj1.CalcY(44); //Correta
+            obj1.CalcY(44, 12); //Correta
+            obj1.CalcY(44, , 34); //Incorreta, arugmento ausente
+
+            Console.WriteLine(obj1.CalcY(1));
+        }
+
+    }
+}
