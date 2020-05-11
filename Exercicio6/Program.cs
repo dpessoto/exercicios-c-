388240e6f3ff1f07c1c7cf0c3a8ca4cfc64c5c92
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-11 10:02:38 -0300

    Exercicio 6

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio6 {
+    enum Frutas {
+        Laranja,
+        Abacaxi, Pera
+    }
+    class Program {
+        static void Main(string[] args) {
+            Frutas Melancia = Frutas.Laranja;
+            switch (Melancia) {
+                case Frutas.Abacaxi:
+                    Console.WriteLine("Pitanga"); //n imprime
+                    break;
+                case Frutas.Pera:
+                    Console.WriteLine("Maça"); //n imprime
+                    break;
+                case Frutas.Laranja:
+                    Console.WriteLine("Jaca"); //imprime
+                    break;
+            }
+        }
+    }
+}
