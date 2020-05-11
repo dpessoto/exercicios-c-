30fc72fb4197e01c5427057dc4c51aeeb8ffe256
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-11 14:02:02 -0300

    Exercicio 15

---
+++ Exercicio15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio15 {
+    class Program {
+        static void Main(string[] args) {
+        }
+
+    }
+
+    class Clock {
+        static Clock relogio;
+        Clock() { }
+        public static Clock Instance() {
+            if (relogio == null) {
+                relogio = new Clock();
+            }
+            return relogio;
+        }
+    }
+}
