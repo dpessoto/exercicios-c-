f769d40e78aa3133990d30e94bf5c8d245f94ce4
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-11 09:50:14 -0300

    Exercicio 5

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio5 {
+    public delegate void Chamar();
+    class C1 {
+        public void M1() {
+            Console.WriteLine("XPTO");
+        }
+        public void M2() {
+            Console.WriteLine("OTPX");
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Chamar p;
+            C1 O = new C1();
+            p = O.M2;
+            p();
+            p = O.M1;
+            p();
+        }
+    }
+}
